fde0310cf2f43a269ba8965aaa12477d7685e336
Allan JARRY <allan.jarry@mistral.fr>
2025-06-20 13:16:28 +0200

    feat(vm): add debug tracing log at intrepretation

--- src/common.h
+++ src/common.h
@@ -4,5 +4,7 @@
 #include <stddef.h>
 #include <stdint.h>
 
+#define DEBUG_TRACE_EXECUTION
+
 #define INTERP_COMMON_H
 #endif
--- src/debug.h
+++ src/debug.h
@@ -2,8 +2,8 @@
 
 #include "chunk.h"
 
-void disassemble_chunk(Chunk* chunk, const char* name);
-int disassemble_instruction(Chunk* chunk, int offset);
+void chunk_disassemble(Chunk* chunk, const char* name);
+int instruction_disassemble(Chunk* chunk, int offset);
 
 #define INTERP_DEBUG_H
 #endif
